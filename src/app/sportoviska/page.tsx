7b8703e432bf54fe515ba462555f510cc4aa7e15
Marin9501 <187885499+Marin9501@users.noreply.github.com>
2024-11-15 14:59:06 +0100

    sportoviska

--- src/app/sportoviska/page.tsx
+++ src/app/sportoviska/page.tsx
@@ -1,5 +1,5 @@
 import { Accordion } from "@/components/ui/accordion";
-import Sportovisko from "./sportovisko";
+import SportoviskoComponent from "./sportovisko";
 import { api } from "@/trpc/server";
 
 export default async function List() {
@@ -8,7 +8,7 @@ export default async function List() {
     <>
       <Accordion type="single" collapsible>
         {sportoviska.map((sportovisko, i) => (
-          <Sportovisko key={i} {...sportovisko} />
+          <SportoviskoComponent key={i} {...sportovisko} />
         ))}
       </Accordion>
     </>
--- src/app/sportoviska/sportovisko.tsx
+++ src/app/sportoviska/sportovisko.tsx
@@ -1,20 +1,38 @@
 import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
-import { Button } from "@/components/ui/button";
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Image from "next/image"
+import { Sportovisko } from "@prisma/client";
+type SportType={
+    id: string,
+    name: string, 
+    info: string, 
+    pictures: {id:string, image:string}[]
+}
 
-
-export default function Sportovisko(props) {
+export default function SportoviskoComponent(props: SportType) {
     return (
-        <AccordionItem value="item-1">
-        <AccordionTrigger> {props.name} </AccordionTrigger>
-        <AccordionContent>
-            {props.info}
-        <Button> Zarezervovat </Button>
-            </AccordionContent>
-        </AccordionItem>
+        <Card className="max-w-[15%] max-h-[15%] border-2">
+            <CardContent>
+                <Image className="pt-10" src={props.pictures[0].image} width='200' height='200' alt='test'/>
+            </CardContent>
+            <CardHeader className="bg-secondary">
+                <CardTitle className="text-center"> {props.name} </CardTitle>
+                <CardDescription> {props.info} </CardDescription>
+            </CardHeader>
+        </Card>
     );
 }
+
+//<AccordionItem value="item-1">
+//<AccordionTrigger> {props.name} </AccordionTrigger>
+//<AccordionContent>
+//{props.info}
+//<Button> Zarezervovat </Button>
+//</AccordionContent>
+//</AccordionItem>
