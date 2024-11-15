c92c1d9bf18d3674681b37b99090258866587676
maximilian.jaroscak <maximilian.jaroscak@uniit.sk>
2024-11-15 15:19:44 +0100

    fix

--- src/app/sportoviska/sportovisko.tsx
+++ src/app/sportoviska/sportovisko.tsx
@@ -6,27 +6,33 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import Image from "next/image"
+import Image from "next/image";
 import { Sportovisko } from "@prisma/client";
-type SportType={
-    id: string,
-    name: string, 
-    info: string, 
-    pictures: {id:string, image:string}[]
-}
+type SportType = {
+  id: string;
+  name: string | null;
+  info: string | null;
+  pictures: { id: string; image: string }[];
+};
 
 export default function SportoviskoComponent(props: SportType) {
-    return (
-        <Card className="max-w-[15%] max-h-[15%] border-2">
-            <CardContent>
-                <Image className="pt-10" src={props.pictures[0].image} width='200' height='200' alt='test'/>
-            </CardContent>
-            <CardHeader className="bg-secondary">
-                <CardTitle className="text-center"> {props.name} </CardTitle>
-                <CardDescription> {props.info} </CardDescription>
-            </CardHeader>
-        </Card>
-    );
+  return (
+    <Card className="max-h-[15%] max-w-[15%] border-2">
+      <CardContent>
+        <Image
+          className="pt-10"
+          src={props.pictures[0]?.image ?? ""}
+          width="200"
+          height="200"
+          alt="test"
+        />
+      </CardContent>
+      <CardHeader className="bg-secondary">
+        <CardTitle className="text-center"> {props.name} </CardTitle>
+        <CardDescription> {props.info} </CardDescription>
+      </CardHeader>
+    </Card>
+  );
 }
 
 //<AccordionItem value="item-1">
