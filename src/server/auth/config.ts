8ec3f64dca6cf6d99a84aa6769fb9032b7ae42f3
maximilian.jaroscak <maximilian.jaroscak@uniit.sk>
2024-11-16 11:26:59 +0100

    feat: google login functionality

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,7 +1,7 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
-
+import GoogleProvider from "next-auth/providers/google";
 import { db } from "@/server/db";
 // import { Role } from "@prisma/client";
 
@@ -33,7 +33,11 @@ declare module "next-auth" {
  */
 export const authConfig = {
   providers: [
-    DiscordProvider,
+    // DiscordProvider,
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
     /**
      * ...add more providers here.
      *
